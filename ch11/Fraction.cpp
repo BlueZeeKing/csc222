3412b432dc73d3ddfd342f958a748b11a0e2e516
Brayden Zee <github@blueish.dev>
2024-11-20 23:04:56 -0500

    Implement constructor

--- ch11/Fraction.cpp
+++ ch11/Fraction.cpp
@@ -1,7 +1,11 @@
 #include "Fraction.h"
 
 Fraction::Fraction() {
+    numerator = 0;
+    denominator = 1;
 }
 
 Fraction::Fraction(int numerator, int denominator) {
+    this->numerator = numerator;
+    this->denominator = denominator;
 }
--- ch11/Fraction.h
+++ ch11/Fraction.h
@@ -1,4 +1,7 @@
 struct Fraction {
+    int numerator;
+    int denominator;
+
     Fraction();
     Fraction(int, int);
 };
