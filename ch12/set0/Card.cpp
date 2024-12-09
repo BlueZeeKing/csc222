b7668bcd7c2cec70a1c40e3ac2c45110fdcffdb7
Brayden Zee <github@blueish.dev>
2024-12-09 12:03:09 -0500

    Implement constructors and to string

--- ch12/set0/Card.cpp
+++ ch12/set0/Card.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 Card::Card() {
+    suit = 0; rank = 0;
 }
 
 Card::Card(int s, int r) {
+    suit = s; rank = r;
 }
 
 string Card::to_string() const {
-    return ""; 
+    string suit_strings[] = {"None", "Clubs", "Diamonds",
+                                   "Hearts", "Spades"};
+    string rank_strings[] = {"Joker", "Ace", "2", "3", "4", "5", "6",
+                                   "7", "8", "9", "10", "Jack", "Queen",
+                                   "King"};
+
+    if (rank == 0) {
+        return rank_strings[rank];
+    }
+
+    return rank_strings[rank] + " of " + suit_strings[suit];
 }
